736f72180045eac978fe069315d54c6cfe5a853d
sathishachilles <sathshh@gmail.com>
2016-09-22 22:45:23 +0000

    show all products

--- lita/lita-help-me-shop/lib/lita/handlers/help_me_shop.rb
+++ lita/lita-help-me-shop/lib/lita/handlers/help_me_shop.rb
@@ -28,22 +28,25 @@ module Lita
         text = nil
         val = req.env['QUERY_STRING']
         parsed_val = JSON.parse(URI.decode_www_form(val)[0][0])
+        
+        message = parsed_val
+        
         if chat_parser.parse(parsed_val)
+          
           text = get_answer(parsed_val['payload'])
         else
           puts "message not addressed at bot"
         end
-        
-        response = 
-        #only if the text is available
-       if text 
-        #psuedo
-        if normal_message
-        reply_with_text(message)
-        elsif button_formatter
-          reply_with_button(message)
-        elsif show_products
-          reply_with_products(message)
+
+        if product_details(message)
+        response = reply_with_products(product_details(message))
+        elsif get_answer(message['payload'])
+          response = reply_with_text(message, get_answer(message['payload']))
+        #elsif 
+         # reply_with_products(message)
+          #generic(products)
+        else
+          response = reply_with_text(message, "Sorry unable to help U")
         end
        end
         puts(response)
@@ -53,8 +56,12 @@ module Lita
         {"user": user,"type": "text","payload": text}
       end
         
-      def reply_with_text(message)
+      def reply_with_text(message, text)
         firebase.push(table_name, text_keygen(message['user'], text))
+      end
+      
+      def reply_with_products(message)
+        firebase.push(table_name, generic(product_details(message)))
       end
 
       def get_response(request)
@@ -99,36 +106,89 @@ module Lita
       def get_quantity(message)
         message
       end
+      
+      def product_details(message)
+        if !products(message).empty
+          products(message)
+        else
+          []
+        end
+      end
 
-
+      def products(message)
+        products = []
+        keys = redis.keys("products_*")
+          keys.each { |k|
+          k_val = redis.hgetall(k)
+            products << k_val if message.match(k_val['category'])
+          }
+          
+          return products
+      end
 
     end
     Lita.register_handler(HelpMeShop)
 
 
-    class ChatParser
+class ChatParser
 
-      def parse message
-        if message['payload']
-          if string_has_dave(message['payload'])
-            return true
-          else
-            return false
-          end
-        else
-          puts "<<<<<<<<<<<<<<<<<<<<<<<<Do  Nothing >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-          return false
-        end
-      end
+  def parse message
+    if ((message['type'].upcase == 'text'.upcase) and message['user'].upcase != 'BOT')
+     call_bot(message['payload']) if string_has_dave(message['payload'])
+   else
+    puts "<<<<<<<<<<<<<<<<<<<<<<<<Do  Nothing >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    end
+  end
 
 
-      def string_has_dave(str)
-        return true if str.include?("Dave") or str.include?("dave")
-      end
+def string_has_dave(str)
+  return true if str.include?("Dave") or str.include?("dave")
+end
 
-      def call_bot(str)
-        puts "<<<<<<<<<<<<<<<<<<,Do Something >>>>>>>>>>>>>>>>....."
-      end
+def call_bot(str)
+  puts "<<<<<<<<<<<<<<<<<<,Do Something >>>>>>>>>>>>>>>>....."
+  base_uri = 'https://tescohack.firebaseio.com/'
+
+  firebase = Firebase::Client.new(base_uri)
+  response = firebase.push("messages", generic)
+  # response = firebase.push("messages", { :user => 'bot ', :type => "Text", :payload => "Hello is it working now ?" })
+end
+
+
+def generic(products)
+  {"type": "GENERIC","user": "bot","payload": elements_array(products)}
+end
+
+def elements_array(products)
+   products_array = add_products(products)
+   {"elements": products_array}.to_s
+end
+
+def button
+  {"user": "bot","type": "BUTTON","payload": '{"text": "What do you want to do next?","buttons": [{"type": "web_url","action": "https://petersapparel.parseapp.com","title": "Show Website"},{"type": "postback","title": "Start Chatting","action": "USER_DEFINED_PAYLOAD"}]}'}
+end
+
+def add_products(products)
+  arr = []
+  products.each do |product|
+    arr << product_info(product)
+  end
+  return arr
+end
+
+def product_info(product)
+  {"title": product['title'],"item_url": product['item_url'],"image_url": product['image_url'],"subtitle": product['subtitle'],"buttons": [{"type": "web_url","title": "View Website","action": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg"},{"type": "postback","title": "Start Chatting","action": "https://petersfancybrownhats.com"}]}
+end
+
+def generic_bkp
+  {"type": "GENERIC","user": "bot","payload": '{"elements": [{"title": "Welcome to Peter s Hats","item_url": "https://petersfancybrownhats.com","image_url": "https://petersfancybrownhats.com/company_image.png","subtitle": "We ve got the right hat for everyone.","buttons": [{"type": "web_url","title": "View Website","action": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg"},{"type": "postback","title": "Start Chatting","action": "https://petersfancybrownhats.com"}]},{"title": "Welcome to Peter s Hats","item_url": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg","image_url": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg","subtitle": "We ve got the right hat for everyone.","buttons": [{"type": "web_url","title": "View Website","action": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg"},{"type": "postback","title": "Start Chatting","action": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg"}]},{"title": "Welcome to Peter s Hats","item_url": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg","image_url": "https://petersfancybrownhats.com/company_image.png","subtitle": "We ve got the right hat for everyone.","buttons": [{"type": "web_url","title": "View Website","action": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg"},{"type": "postback","title": "Start Chatting","action": "http://img.tesco.com/Groceries/pi/457/5000436589457/IDShot_225x225.jpg"}]}]}'}
+end
+
+
+def button_bkp
+  {"user": "bot","type": "BUTTON","payload": '{"text": "What do you want to do next?","buttons": [{"type": "web_url","action": "https://petersapparel.parseapp.com","title": "Show Website"},{"type": "postback","title": "Start Chatting","action": "USER_DEFINED_PAYLOAD"}]}'}
+end
+
 
     end
 
